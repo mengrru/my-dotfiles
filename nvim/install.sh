c66ad68565b4da47825ae82e9d4683986d11c70d
mengrru <mengrru@outlook.com>
2021-10-12 20:25:47 +0800

    update

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,7 +4,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 # install latest node
 curl -sL install-node.now.sh/lts | sudo bash && \
 
-mkdir ../coc/extensions && cp ./package.json ../coc/extensions/package.json && cd ../coc/extensions && \
+mkdir ../coc && mkdir ../coc/extensions && cp ./package.json ../coc/extensions/package.json && cd ../coc/extensions && \
 # install coc extensions
 npm i && \
 # init nvim
